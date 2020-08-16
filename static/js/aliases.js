d06f3c3c9c3cb4c021bc346dea93d6b3b5c57882
Stephan Cilliers <stephanus.cilliers@protonmail.com>
2020-08-16 02:31:25 +0200

    Clean up

--- static/js/aliases.js
+++ static/js/aliases.js
@@ -5,7 +5,6 @@ emailElements.forEach(e => {
         document.execCommand("copy");
     }
     e.addEventListener("copy", function(event) {
-        console.log("hello")
           event.preventDefault();
           if (event.clipboardData) {
             event.clipboardData.setData("text/plain", e.querySelector("span").textContent);
