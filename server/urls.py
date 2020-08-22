2a5370a802eb92e92c9561acd18d2bc1008b399b
Stephan Cilliers <stephanus.cilliers@gmail.com>
2020-08-22 18:58:07 +0200

    Privacy policy, terms of service

--- server/urls.py
+++ server/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import PasswordResetView
+from django.views.generic.base import TemplateView 
 from functools import wraps
 
 from . import views
@@ -8,6 +9,8 @@ from . import forms
 
 urlpatterns = [
     path('', views.AliasPage.as_view(), name='home'),
+    path('terms-of-service/', TemplateView.as_view(template_name='terms-of-service.html'), name='terms_of_service'),
+    path('privacy/', TemplateView.as_view(template_name='privacy-policy.html'), name='privacy_policy'),
     path('alias/<int:alias_id>/<str:method>', views.AliasAction.as_view(), name='alias_action'),
     path('settings/', views.SettingsPage.as_view(), name='settings'),
     path('settings/resend_activation', views.SettingsPage.as_view(), name='resend_activation'),
