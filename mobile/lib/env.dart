10b199fefcedbcc0435a8745bd066b2c76dead6b
Stephan Cilliers <stephanus.cilliers@protonmail.com>
2021-12-23 16:01:50 +0200

    stash

--- mobile/lib/env.dart
+++ mobile/lib/env.dart
@@ -1,8 +1,8 @@
 const bool isProduction = bool.fromEnvironment("dart.vm.product");
 
 const testConfig = {
-  // "SERVER_ENDPOINT": "https://forwardmail.herokuapp.com/api/v1"
-  "SERVER_ENDPOINT": "http://10.0.2.2:8080/api/v1"
+  "SERVER_ENDPOINT": "https://forwardmail.herokuapp.com/api/v1"
+  // "SERVER_ENDPOINT": "http://10.0.2.2:8080/api/v1"
 };
 
 const productionConfig = {
